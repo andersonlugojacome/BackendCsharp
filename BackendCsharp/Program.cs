5f37efbda88c97bfee9b7ff0180a90f51969a27e
Anderson Lugo <andersonlugojacome@hotmail.com>
2024-06-26 16:07:17 -0500

    init

--- BackendCsharp/Program.cs
+++ BackendCsharp/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!, DigitalesWeb");
+Sale sale = new Sale();
+    sale.total = 15;
+Console.WriteLine("Sale cuesta: " + sale.total.ToString());
+
+
+class Sale
+{
+    public decimal total { get; set; }
+
+}
 
